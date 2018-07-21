7a411ba51102d3492859c18fdfb4db9f93431f3e
Mark <bucknema@oregonstate.edu>
2018-07-20 20:54:15 -0400

    Add level2 .tsx + .tmx files

---
+++ data/map/level2/Castles.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Castles" tilewidth="248" tileheight="308" tilecount="2" columns="0">
+ <tileoffset x="-64" y="32"/>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="248" height="308" source="../../img/tiles/color0_Castle_Huge_face1_0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="248" height="308" source="../../img/tiles/color6_Castle_Huge_face2_1.png"/>
+ </tile>
+</tileset>
---
+++ data/map/level2/Generators.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Generators" tilewidth="248" tileheight="308" tilecount="2" columns="0">
+ <tileoffset x="-64" y="32"/>
+ <grid orientation="isometric" width="1" height="1"/>
+ <tile id="0">
+  <image width="248" height="308" source="../../img/tiles/color5_Factory_Huge_face2_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="248" height="308" source="../../img/tiles/color5_Factory_Huge_face3_0.png"/>
+ </tile>
+</tileset>
